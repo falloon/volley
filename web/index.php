a62d851de6c4299f8b8286ec0da757b37456172e
falloon <falloon@college.harvard.edu>
2015-04-15 19:51:49 -0400

    postgres..

--- web/index.php
+++ web/index.php
@@ -1,5 +1,23 @@
+
+
 <?php
-
-	include_once("opportunities2.html");
-
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  return "dbname=dd76qbdlfmsh69 host=ec2-54-204-36-244.compute-1.amazonaws.com port=5432 user=wagshuhjlacogd password=0Z_mGmhfa98wjV7ZMEnf4coVj3 sslmode=require";
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error."
+    exit;
+}
+echo "hello";
+// $result = pg_query($db, "SELECT statement goes here");
+// include_once("opportunities2.html");
 ?>
+
+
+	
+
