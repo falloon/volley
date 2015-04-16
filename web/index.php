faeeb3c81791148f431dc7eac8798fb16b9b6062
falloon <falloon@college.harvard.edu>
2015-04-15 21:11:52 -0400

    Postgres working?

--- web/index.php
+++ web/index.php
@@ -1,25 +1,60 @@
-
-
 <?php
-echo "<html><body><h1>hello</h1></body?</html>";
 
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  return "dbname=dd76qbdlfmsh69 host=ec2-54-204-36-244.compute-1.amazonaws.com port=5432 user=wagshuhjlacogd password=0Z_mGmhfa98wjV7ZMEnf4coVj3 sslmode=require";
-}
- 
-# Establish db connection
-$db = pg_connect(pg_connection_string());
-if (!$db) {
-    echo "Database connection error."
-    exit;
-}
+require('../vendor/autoload.php');
 
-// $result = pg_query($db, "SELECT statement goes here");
-// include_once("opportunities2.html");
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register the Twig templating engine
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/../views',
+));
+
+// Register the Postgres database add-on
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+  array(
+    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+    'pdo.port' => $dbopts["port"],
+    'pdo.username' => $dbopts["user"],
+    'pdo.password' => $dbopts["pass"]
+  )
+);
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return 'Hello';
+});
+
+$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
+
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
+
+$app->get('/twig/{name}', function($name) use($app) {
+  return $app['twig']->render('index.twig', array(
+    'name' => $name,
+  ));
+});
+
+
+$app->run();
+
 ?>
-
-
-	
-
